f0c3553f514d55d4beb1dc8bda25418aafce58a5
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-05-14 15:03:01 +0200

    fix?

--- src/dtb/dbconnection.js
+++ src/dtb/dbconnection.js
@@ -1,13 +1,37 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require('mysql2')
+const logger = require('../utils/logger')
+require('dotenv').config()
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
-module.exports = pool;
+    connectionLimit: 10,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    multipleStatements: true
+}
+
+logger.trace(dbConfig)
+
+const pool = mysql.createPool(dbConfig)
+
+pool.on('connection', function (connection) {
+    logger.trace(
+        `Connected to database '${connection.config.database}' on '${connection.config.host}:${connection.config.port}'`
+    )
+})
+
+pool.on('acquire', function (connection) {
+    logger.trace('Connection %d acquired', connection.threadId)
+})
+
+pool.on('release', function (connection) {
+    logger.trace('Connection %d released', connection.threadId)
+})
+
+module.exports = pool
