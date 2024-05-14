672e4cdbff4812ec355d2bf34a7d489f2a0f7dcc
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2024-05-14 16:03:21 +0200

    test

--- src/dtb/dbconnection.js
+++ src/dtb/dbconnection.js
@@ -8,7 +8,7 @@ const dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-
+    
     connectionLimit: 10,
     waitForConnections: true,
     connectionLimit: 10,
